c16049ee7542ad580837bf39bfd783744f42cf7d
Wil <wilsonsecord@gmail.com>
2026-03-30 12:51:10 -0400

    testing

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 pacman -Syyu fzf zoxide zsh docker bat neovim npm pyenv pv git
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "$(curl -fsSL https://raw.githubusercontent.com/wilSecord/dotfiles/refs/heads/main/.zshrc)" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
