191ff4003f470e0551344d0fcfe60b459a6000d7
Wil <wilsonsecord@gmail.com>
2026-03-30 12:39:38 -0400

    readme command and fixed zshrc location

---
+++ README.md
@@ -0,0 +1,4 @@
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilSecord/arch-setup/refs/heads/main/setup.sh)"
+```
+
--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 
 pacman -Syyu fzf zoxide zsh docker bat neovim npm pyenv pv git
-touch ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "$(curl -fsSL https://raw.githubusercontent.com/wilSecord/dotfiles/refs/heads/main/.zshrc)" > .zshrc
+echo "$(curl -fsSL https://raw.githubusercontent.com/wilSecord/dotfiles/refs/heads/main/.zshrc)" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions
