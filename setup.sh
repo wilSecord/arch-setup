fa980ed22a890b37a96482badb912e3e9591ec83
Wil <wilsonsecord@gmail.com>
2026-03-30 12:29:01 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
-pacman -Syyu fzf zoxide zsh docker bat neovim npm pyenv
+pacman -Syyu fzf zoxide zsh docker bat neovim npm pyenv pv
+touch ~/.zshrc
+"$(curl -fsSL https://raw.githubusercontent.com/wilSecord/dotfiles/refs/heads/main/.zshrc)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlighting
